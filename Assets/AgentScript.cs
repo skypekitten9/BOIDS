aae45efb66e3213ac020a378d4dad1afd83874b1
skypekitten9 <victor.enhorning@gmail.com>
2020-01-14 13:10:39 +0100

    Created a randezvous point, cage, flock and agent prefab.

---
+++ Assets/AgentScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentScript : MonoBehaviour
+{
+    public float speed;
+    public float sight;
+    float privateRange;
+
+    private void Start()
+    {
+        privateRange = sight / 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
